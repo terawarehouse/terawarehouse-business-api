21506fbee6d7c22f0ba209d31d6b941f165e7e5b
Edward P. Legaspi <czetsuya@gmail.com>
2019-11-01 08:24:18 +0800

    Initial commit.

---
+++ src/main/java/com/terawarehouse/gateway/TerawarehouseGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.terawarehouse.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerawarehouseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerawarehouseGatewayApplication.class, args);
+	}
+
+}
