ec9b163fe65708df6739c6cd372f723eb2dee376
Edward P. Legaspi <czetsuya@gmail.com>
2019-11-02 11:02:07 +0800

    Update readme.

--- src/main/java/com/terawarehouse/gateway/TerawarehouseGatewayApplication.java
+++ src/main/java/com/terawarehouse/gateway/TerawarehouseGatewayApplication.java
@@ -2,12 +2,24 @@ package com.terawarehouse.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class TerawarehouseGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TerawarehouseGatewayApplication.class, args);
 	}
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
